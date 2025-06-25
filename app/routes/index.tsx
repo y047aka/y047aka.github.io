d9b3ebf15ad78b13ee97c693b790007e9780ca52
Yoshitaka Totsuka <y047aka@gmail.com>
2025-06-25 10:26:18 +0900

    refactor: 記事を投稿日の新しい順でソート

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -9,19 +9,26 @@ export default createRoute((c) => {
     eager: true
   })
 
+  // 記事を投稿日の新しい順でソート
+  const sortedPosts = Object.entries(posts)
+    .filter(([, module]) => module.frontmatter)
+    .sort(([, a], [, b]) => {
+      const dateA = new Date(a.frontmatter!.pubDate)
+      const dateB = new Date(b.frontmatter!.pubDate)
+      return dateB.getTime() - dateA.getTime() // 新しい順（降順）
+    })
+
   return c.render(
     <TopSection title="Blog Posts">
       <div class={css`display: flex; flex-direction: column; row-gap: 5px;`}>
-        {Object.entries(posts).map(([id, module]) => {
-          if (module.frontmatter) {
-            return (
-              <LinkTile
-                title={module.frontmatter.title}
-                subTitle={module.frontmatter.pubDate}
-                url={`${id.replace(/\.md$/, '')}`}
-              />
-            )
-          }
+        {sortedPosts.map(([id, module]) => {
+          return (
+            <LinkTile
+              title={module.frontmatter!.title}
+              subTitle={module.frontmatter!.pubDate}
+              url={`${id.replace(/\.md$/, '')}`}
+            />
+          )
         })}
       </div>
     </TopSection>
