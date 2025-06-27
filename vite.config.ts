700993e92e61095fd7004bcda627fbeb0353cb01
Yoshitaka Totsuka <y047aka@users.noreply.github.com>
2025-06-27 22:22:43 +0900

    feat: fp-matsuri_2025 (#22)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import ssg from '@hono/vite-ssg'
 import mdx from '@mdx-js/rollup'
 import honox from 'honox/vite'
+import { devServerDefaultOptions } from 'honox/vite'
 import remarkFrontmatter from 'remark-frontmatter'
 import remarkGfm from 'remark-gfm'
 import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
@@ -11,7 +12,16 @@ const entry = './app/server.ts'
 export default defineConfig(() => {
   return {
     plugins: [
-      honox(),
+      honox({
+        devServer: {
+          exclude: [
+            ...devServerDefaultOptions.exclude,
+            /^\/app\/.+/,
+            /^\/favicon.ico$/,
+            /\.(png|jpg|jpeg|gif|svg|webp|ico|css|js|ts|json)$/,
+          ],
+        },
+      }),
       ssg({ entry }),
       mdx({
         jsxImportSource: 'hono/jsx',
